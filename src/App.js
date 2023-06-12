09d4118dfdf106e9e974a28c7a9ae7ec8ad4d881
big-case <bikeshmhrjn@proton.me>
2023-06-12 21:34:06 +0545

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AddBook from "./components/AddBook";
+import Book from "./components/Book";
+import BooksList from "./components/BookList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/books" className="navbar-brand">
+          Books
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/books"} className="nav-link">
+              Book List
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<BooksList/>} />
+          <Route path="/books" element={<BooksList/>} />
+          <Route path="/add" element={<AddBook/>} />
+          <Route path="/books/:id" element={<Book/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
